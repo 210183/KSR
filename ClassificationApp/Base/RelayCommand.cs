a803aed48e70a4c5f166352f9cba5b8ba371c8b8
210241 <210241@edu.p.lodz.pl>
2019-03-31 22:22:00 +0200

    WPF application with basic window and viewmodel

---
+++ ClassificationApp/Base/RelayCommand.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ClassificationApp.Base
+{
+    class RelayCommand : IRaiseCanExecuteCommand
+    {
+        private readonly Action _targetExecuteMethod;
+        private readonly Func<bool> _targetCanExecuteMethod;
+
+        public event EventHandler CanExecuteChanged = delegate { };
+
+        public RelayCommand(Action executeMethod)
+        {
+            _targetExecuteMethod = executeMethod;
+        }
+
+        public RelayCommand(Action executeMethod, Func<bool> canExecuteMethod)
+        {
+            _targetExecuteMethod = executeMethod;
+            _targetCanExecuteMethod = canExecuteMethod;
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged(this, EventArgs.Empty);
+        }
+
+        #region ICommand Members
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            if (_targetCanExecuteMethod != null)
+            {
+                return _targetCanExecuteMethod();
+            }
+
+            return _targetExecuteMethod != null;
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            _targetExecuteMethod?.Invoke();
+        }
+
+        #endregion ICommand Members
+    }
+}
---
+++ ClassificationApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassificationApp.Base;
+using FileSamplesRead;
+using FileSamplesRead.Models;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
+
+namespace ClassificationApp.ViewModels
+{
+    class MainViewModel : BindableBase
+    {
+        private decimal _percentageOfLearningFiles;
+        private string _labelName;
+        private int _nearestNeighboursNumber;
+        private int _coldStartSamples;
+        //private ExtractorType _extractorType;
+        private string _directoryFilePath;
+        private int _filesInDirectory;
+        private List<string> _listOfFiles;
+        private List<RawSample> _listOfRawSamples;
+
+        #region observable props
+        public decimal PercentageOfLearningFiles
+        {
+            get => _percentageOfLearningFiles;
+            set => SetProperty(ref _percentageOfLearningFiles, value);
+        }
+        public string LabelName
+        {
+            get => _labelName;
+            set => SetProperty(ref _labelName, value);
+        }
+        public int NearestNeighboursNumber
+        {
+            get => _nearestNeighboursNumber;
+            set => SetProperty(ref _nearestNeighboursNumber, value);
+        }
+        public int ColdStartSamples
+        {
+            get => _coldStartSamples;
+            set => SetProperty(ref _coldStartSamples, value);
+        }
+        //public ExtractorType ExtractorType
+        //{
+        //    get => _extractorType;
+        //    set => SetProperty(ref _extractorType, value);
+        //}
+        public string DirectoryFilePath
+        {
+            get => _directoryFilePath;
+            private set => SetProperty(ref _directoryFilePath, value);
+        }
+        public int FilesInDirectory
+        {
+            get => _filesInDirectory;
+            private set => SetProperty(ref _filesInDirectory, value);
+        }
+        #endregion
+
+        #region Commands
+        public IRaiseCanExecuteCommand OpenDirectoryCommand { get; }
+        public IRaiseCanExecuteCommand LoadFilesCommand { get; }
+        public IRaiseCanExecuteCommand ExtractCommand { get; }
+        public IRaiseCanExecuteCommand ClassifyCommand { get; }
+        public IRaiseCanExecuteCommand ResultCommand { get; }
+        #endregion
+
+        public MainViewModel()
+        {
+            OpenDirectoryCommand = new RelayCommand(ReadDirectoryPath);
+            LoadFilesCommand = new RelayCommand(LoadFiles);
+            ExtractCommand = new RelayCommand(ExtractFromSamples);
+            ClassifyCommand = new RelayCommand(ClassifySamples);
+            ResultCommand = new RelayCommand(ShowResults);
+        }
+
+
+        private void ReadDirectoryPath()
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            folderDialog.ShowDialog();
+            DirectoryFilePath = folderDialog.SelectedPath;
+
+            _listOfFiles = Directory.GetFiles(DirectoryFilePath).Where(p => Path.GetExtension(p) == "sgm").ToList();
+            FilesInDirectory = _listOfFiles.Count;
+
+        }
+
+        private void LoadFiles()
+        {
+            var dataReader = new DataSamplesReader();
+            _listOfRawSamples = new List<RawSample>();
+            foreach (string path in _listOfFiles)
+            {
+                _listOfRawSamples = dataReader.ReadAllSamples(path);
+            }
+        }
+
+        private void ExtractFromSamples()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ClassifySamples()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ShowResults()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
