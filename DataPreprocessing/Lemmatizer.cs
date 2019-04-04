fc14e89788c84141d2763d50d3f58799f36308ae
210241 <210241@edu.p.lodz.pl>
2019-04-04 19:17:30 +0200

    Lemmatizer base

---
+++ DataPreprocessing/Lemmatizer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FileSamplesRead.Models;
+
+namespace DataPreprocessing
+{
+    static class Lemmatizer
+    {
+        public List<PreProcessedSample> LemmatizeSet(List<PreProcessedSample> sample)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string LemmatizeOne(string word)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ DataPreprocessing/PreProcessedSample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Models.Concrete;
+
+namespace DataPreprocessing
+{
+    public class PreProcessedSample
+    {
+        public PreProcessedSample(LabelsCollection labelsCollection, List<string> words)
+        {
+            LabelsCollection = labelsCollection ?? throw new ArgumentNullException(nameof(labelsCollection));
+            Words = words ?? throw new ArgumentNullException(nameof(words));
+        }
+
+        public LabelsCollection LabelsCollection { get; set; }
+        public List<string> Words { get; set; }
+
+
+    }
+}
